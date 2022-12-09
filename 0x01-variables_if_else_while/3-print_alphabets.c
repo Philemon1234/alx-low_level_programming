7bf9bd0fe83bd928ea662405da3e8cac65a31211
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-12-09 14:14:17 -0800

    Added 3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there*/
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+/* My  code */
+char c;
+char d;
+
+c = 'a';
+d = 'A';
+while
+(c <= 'z') {
+putcher(c);
+c++;
+}
+while
+(d <= 'z') {
+putcher(d);
+d++;
+}
+putcher("\n");
+return (0);
+}
