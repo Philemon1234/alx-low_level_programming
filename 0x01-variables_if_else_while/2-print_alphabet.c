d5de0706e845210378b398408a155bc602293a5b
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-12-09 13:30:33 -0800

    Added 2-print_alphabet.c  file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there*/
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n;
+/* My char code */
+char c;
+c = 'a';
+while
+(c <= 'z') {
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
