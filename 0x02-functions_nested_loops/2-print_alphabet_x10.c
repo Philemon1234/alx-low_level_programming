37ccf1ae782263450aa40ca89d46bd298d1cc51c
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-12-14 10:26:34 -0800

    Added 2-print_alphabet_x10.c file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,24 @@
+#include "main.h"
 
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 1;
+	char y = 'a';
+	while (x <= 10)
+	{
+		while (y <= 'z')
+		{
+			_putchar(y);
+			y++;
+		}
+		y = 'a';
+		_putchar('\n');
+		x++;
+	}
+}
