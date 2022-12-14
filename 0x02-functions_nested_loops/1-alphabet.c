16b9e43f3dfbf67c0b1340d4db0c070ef4d26625
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-12-14 09:27:59 -0800

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+char n;
+for (n = 'a; n <= 'z'; n++)
+_putchar(n);
+_putchar('\n');
+return (0);
+}
