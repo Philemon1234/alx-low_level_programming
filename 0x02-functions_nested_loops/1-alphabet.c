254144a48de30ca73ea9ac5beb98a23ac348af96
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2022-12-14 09:46:47 -0800

    Added _putchar.c fil

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * print_alphabet - writes the character c to stdout
- *   *
- *    * Return: 0
- *     */
+ * print_alphabet - writes the character c to stdout
+ *
+ * Return: 0
+ */
 
 void print_alphabet(void)
 {
