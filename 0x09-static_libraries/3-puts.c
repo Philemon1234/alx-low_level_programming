64ce6becab3742f3276cdfd4df8a875f897ae72a
Philemon1234 <ntivuguruzwaphilemon0@gmail.com>
2023-01-10 10:21:55 -0800

    Created static library libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
- /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-int main(void)
+void _puts(char *s)
+#include <stdio.h>
+	/**
+	 * main - Entry point
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
+.
